cebe292e1f5845ec01cae83c59b5f832bc025c5c
Michel Sunsin <michel.fulltime@outlook.com>
2019-11-19 18:03:04 -0300

    Routing and user registration

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
     res.json({ msg: 'Welcome to the ContactKeeper API...' });
 });
 
-//Define routes
+// Define routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contact', require('./routes/contact'));
+app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 3000;
